924120935f52821a0cb0530197a776b1230541f9
unknown <reinaldoagf1@gmail.com>
2022-08-31 18:50:09 -0400

    Seccion de 'portfolio'

--- js/app.js
+++ js/app.js
@@ -33,3 +33,42 @@ photoContent.addEventListener("mouseout", () =>{
          iterator.style.transition = "ease-in .25s";
     }
 }, false);
+
+
+let scrollerID;
+let paused = true;
+let interval = 10;
+
+function startScroll(){
+    let id = setInterval(function() {
+     const horizontalChild = document.querySelector('#horizontal-child')
+     horizontalChild.scrollBy(0, 1);
+     const styleHorizontalChild = getComputedStyle(horizontalChild)
+               if ((parseInt(styleHorizontalChild['height'])  + horizontalChild.scrollTop) >= (horizontalChild.scrollHeight+15)) {
+                    // end of page?
+                    stopScroll();
+                }
+    }, interval);
+    return id;
+}
+
+function stopScroll() {
+     console.log('stopScroll')
+     const iconPlayBtn = document.querySelector("#icon-play-btn")
+     paused = true;
+     iconPlayBtn.className = "fa fa-play";
+    clearInterval(scrollerID);
+}
+function toggleScroll() {
+     const iconPlayBtn = document.querySelector("#icon-play-btn")
+     if(paused == true) {
+          scrollerID = startScroll();
+          paused = false;
+          iconPlayBtn.className = "fa fa-pause";
+     } else {
+          stopScroll();
+          paused = true;
+          iconPlayBtn.className = "fa fa-play";
+     }
+}
+
